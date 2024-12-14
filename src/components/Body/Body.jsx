59649d9befb3807bc41cc92052172bf0837c504f
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2024-12-13 23:04:08 -0600

    cardEdit

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,12 +1,60 @@
 import React from "react";
 import "./Body.css";  // Si deseas agregar estilos personalizados
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardActions from '@mui/material/CardActions';
 
 const Body = () => {
   return (
     <div className="body-container">
-      <div className="left-column">Columna 1 (3fr)</div>
-      <div className="middle-column">Columna 2 (1fr)</div>
-      <div className="right-column">Columna 3 (2fr)</div>
+     <div className="left-column">
+        {/* Card reemplazando el contenido de left-column */}
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://via.placeholder.com/345x140" // Aquí pon la imagen que quieras
+              alt="Dojo Roma"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Dojo Roma
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                Conoce Nuestro Aikido Dojo, el centro de comunión en el área centro.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary" href="#">
+              Dojo Roma Site
+            </Button>
+            <Button size="small" color="primary" href="#">
+              Únetenos
+            </Button>
+            <Button size="small" color="primary" href="#">
+              Ubicación
+            </Button>
+          </CardActions>
+        </Card>
+      </div>
+
+      <div className="middle-column">
+        <h2>Que es Aikido?</h2>
+        <p>
+          Aikido es el Arte Marcial creado por nuestro fundador Morihei Ueshiba
+        </p>
+      </div>
+
+      <div className="right-column">
+        <h3>Clases de Aikido</h3>
+        <h3>Clases Especiales</h3>
+      </div>
     </div>
   );
 };
