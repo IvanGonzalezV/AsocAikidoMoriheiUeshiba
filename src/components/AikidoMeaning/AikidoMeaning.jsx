f2883b31a9c3cf7b9afe29f667669328753ff29d
Ivan Gonzalez Vargas <ronin7_@hotmail.com>
2024-12-27 19:35:16 -0600

    aikidoMeaning_1

--- src/components/AikidoMeaning/AikidoMeaning.jsx
+++ src/components/AikidoMeaning/AikidoMeaning.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./AikidoMeaning.css"; // Puedes agregar estilos personalizados aquí si lo deseas.
+import O from "../../assets/O-removebg.png"
 
 const AikidoMeaning = () => {
   return (
+    <div className="aikido-meaning-container">
+    <img src={O} alt="O-removebg.png" />
     <div className="aikido-meaning">
       <h1>¿Qué es AIKIDO?</h1>
       <p>
@@ -11,7 +14,9 @@ const AikidoMeaning = () => {
         enfoque principal es la paz y la resolución de conflictos sin violencia.
       </p>
     </div>
+    </div>
   );
 };
 
 export default AikidoMeaning;
+
